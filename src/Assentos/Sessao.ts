7bf24cca44745daa6d5d5bc5e7182f289386490f
Rodrigo Gomes <rodrigmatrix@hotmail.com>
2018-10-01 23:32:36 -0300

    Ajuste no container

--- src/Assentos/Sessao.ts
+++ src/Assentos/Sessao.ts
@@ -1,30 +1,17 @@
-class Sessao extends Sala{
-    
+class Sessao{
+    data: string;
+    horario: string;
+    numeroSala: number;
+    cadeiras: number;
+    filme: string;
     constructor(data,horario,numeroSala,cadeiras,filme){
         this.data = data;
         this.horario = horario;
         this.numeroSala = numeroSala;
         this.cadeiras = cadeiras;
         this.filme = filme;
-        cadeirasArray[cadeiras];
     } 
 
-    comprarIngresso(userid,cadeira){
-        if(cadeirasArray[cadeira]!=null){
-            cadeirasArray[cadeira]=userid;
-        }
-        else{
-            M.toast({html: 'Assento já está ocupada'}); 
-        }     
-    }
 
-    cancelarCompra(cadeira){
-        if(cadeirasArray[cadeira]!=null){
-            cadeirasArray[cadeira]=null;
-        }
-        else{
-            M.toast({html: 'Assento não está ocupado'}); 
-        }
-    }
 
 }
