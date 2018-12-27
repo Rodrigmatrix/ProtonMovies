b6066fb90a27c375a26dc3c649d39447f54265b0
Rodrigmatrix <rodrigmatrix@hotmail.com>
2018-12-27 00:07:37 -0300

    Seleção e filtro preço de ingresso funcional

--- src/Seats.js
+++ src/Seats.js
@@ -1,8 +1,10 @@
 var selectedArray = new Array();
 var size=0;
+var lastSelectedPrice;
+var lastSelectedHalfPrice;
 function setSeats(session){
     //console.log(session);
-    console.log("teste "+session.seats[0].client_id);
+    //console.log("teste "+session.seats[0].client_id);
     for(var i=0;i<=49;i++){
         var value = session.seats[i].client_id;
         if(value == null){
@@ -14,7 +16,7 @@ function setSeats(session){
             selectedArray[i+1]=-1;
         }
     }
-    console.log("array "+selectedArray);
+    //console.log("array "+selectedArray);
 }
 
 function rated(movie_rated){
@@ -50,10 +52,11 @@ function showSessionInfo(session,movie){
 
     <div class="col-content s4">
       <p class=" center flow-text">${movie.name}</p>
-      <p><label for=""></label>${session.auditorium}</p>
+      
       <p><label for="">Duração: </label>${movie.runtime +" min"}</p>
       <p><label for="">Horário: </label>${session.time}</p>
       <p><label for="">Data: </label>${session.date}</p>
+      <p><label for=""></label>${session.auditorium}</p>
       <p><label class="center"></label>${session.image_type}</p>
       <img src="${rated(movie.rated)}" class="center"
         style="height: 30px; width: 30px;">
@@ -66,24 +69,25 @@ function showSessionInfo(session,movie){
 function convertSeat(seat){
     seat = seat.toString();
     seat = seat.split("");
-    console.log("assento "+seat);
-    var size = seat.lenght;
-    if(size == 1){
+   // console.log("assento "+seat);
+    var size = seat.length;
+   // console.log("size "+size);
+    if(seat.length == 1){
         return "A"+seat[0];
     }
-    else if(seat[1] == 0){
+    if(seat[1] == 0){
         switch(seat[0]){
             case "1":
             return "A 10";
 
             case "2":
-            return "C 20";
+            return "B 20";
 
             case "3":
-            return "D 30";
+            return "C 30";
 
             case "4":
-            return "E 40";
+            return "D 40";
 
             case "5":
             return "E 50";
@@ -99,23 +103,23 @@ function convertSeat(seat){
             return "B "+seat[1];
 
             case "2":
-            return "C "+seat[0]+seat[1];
+            return "C "+seat[1];
 
             case "3":
-            return "D "+seat[0]+seat[1];
+            return "D "+seat[1];
 
             case "4":
-            return "E "+seat[0]+seat[1];
+            return "E "+seat[1];
 
             case "5":
-            return "F "+seat[0]+seat[1];
+            return "F "+seat[1];
 
         }
     }
 }
 
 function printSelected(seat){
-    console.log(selectedArray);
+    //console.log(selectedArray);
     var converted = convertSeat(seat);
         document.getElementById("noneSelected").innerHTML=( `
        
@@ -127,17 +131,219 @@ function printSelected(seat){
         `);
 }
 
+async function selectPrice(type){
+    // ta foda...
+    var fullPrice = await parseInt(document.getElementById("price").value, 10);
+    var halfPrice = await parseInt(document.getElementById("halfPrice").value, 10);
+    lastSelectedHalfPrice = halfPrice;
+    lastSelectedPrice = fullPrice;
+    var fullSelected = await parseInt(fullPrice+halfPrice, 10);
+    console.log("full price "+fullSelected);
+    $("#btnCheckout").html(`
+            <a class="blue waves-effect waves-light right btn disabled" onclick="selectTicketsPrice()">Proseguir para pagamento</a>
+            `);
+    //console.log("ultima inteira selecionada depois "+lastSelectedPrice);
+    //console.log("ultima meia selecionada depois "+lastSelectedPrice);
+    if(type == 0){
+        var qtd = size-fullPrice;
+        //console.log("selecionado full "+fullPrice);
+        if(fullPrice == size){
+            $("#btnCheckout").html(`
+            <a class="blue waves-effect waves-light right btn " onclick="selectTicketsPrice()">Proseguir para pagamento</a>
+            `);
+        }
+        if(fullSelected == size){
+            //console.log("rthgfthtrhtr");
+            $("#btnCheckout").html(`
+            <a class="blue waves-effect waves-light right btn " onclick="selectTicketsPrice()">Proseguir para pagamento</a>
+            `);
+        }
+        for(var i=0;i<=qtd;i++){
+            if(i == 0){
+                document.getElementById("halfPrice").innerHTML=( `
+                <option value="0">0</option>
+                `);
+            }
+            else{
+                document.getElementById("halfPrice").innerHTML+=( `
+                <option value="${i}">${i}</option>
+                `);
+                
+            }
+        }
+        document.getElementById("halfPrice").value = lastSelectedHalfPrice;
+    }
+    else{
+        await console.log("full "+fullSelected);
+        var qtd = size-halfPrice;
+        //console.log("selecionado half "+halfPrice);
+        if(halfPrice == size){
+            //console.log("desativar o outro");
+            $("#btnCheckout").html(`
+            <a class="blue waves-effect waves-light right btn " onclick="selectTicketsPrice()">Proseguir para pagamento</a>
+            `);
+        }
+        else if(fullSelected == size){
+            $("#btnCheckout").html(`
+            <a class="blue waves-effect waves-light right btn " onclick="selectTicketsPrice()">Proseguir para pagamento</a>
+            `);
+
+        }
+        for(var i=0;i<=qtd;i++){
+            //console.log("i");
+
+            if(i == 0){
+                document.getElementById("price").innerHTML=( `
+                <option value="0">0</option>
+                `);
+            }
+            else{
+                document.getElementById("price").innerHTML+=( `
+                <option value="${i}">${i}</option>
+                `);
+            }
+        }
+        document.getElementById("price").value = lastSelectedPrice;
+     
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('select');
+        var instances = M.FormSelect.init(elems, options);
+      });
+    
+      // Or with jQuery
+    
+      $(document).ready(function () {
+        $('select').formSelect();
+      });
+
+}
+
+
+
+function showSelectedTicket(){
+    var strSeats = "";
+    for(var i=1;i<=50;i++){
+        if(selectedArray[i] != null && selectedArray[i] != -1){
+            strSeats+=`${convertSeat(selectedArray[i])}, `;
+        }
+    }
+    //console.log("str "+strSeats);
+    document.getElementById("modalSelectTickets").innerHTML=( `
+    <!-- Modal Structure -->
+    <div id="modal3" class="modal bottom-sheet" tabindex="0" style="z-index: 1003; display: none; opacity: 0; bottom: -100%; ">
+    <div class="modal-content">
+      <div class="row">
+        <div class="col s9">
+            <h3 class="header">Selecione o tipo do ingresso</h3>
+        </div>
+
+        <div class="col s3">
+            <h4 class="header">Selecionados: ${strSeats}</h4>
+        </div>
+      </div>
+      
+      
+      <ul class="collection">
+
+        <li class="collection-item "  style="white-space: nowrap;">
+
+          <span class="title">Inteira</span>
+          <p>R$: 20
+            <br>
+            <br>
+          </p>
+          <div class="input-field  right">
+              <select id="price" onchange="selectPrice(${0})">
+              <option value="0">0</option>
+              </select>
+              <label>Quantidade</label>
+            </div>
+        </li>
+
+          <li class="collection-item  " style="white-space: nowrap;">
+            <span class="title">Meia</span>
+            <p>R$: 20
+              <br> <label> Necessário a apresentação de comprovante na entrada da sala</label>
+            </p>
+            <div class="input-field  right">
+              <select  id="halfPrice" onchange="selectPrice(${1})">
+              <option value="0">0</option>
+              </select>
+              <label>Quantidade</label>
+            </div>
+            
+          </li>
+    </div>
+    </ul>
+    <div id="btnCheckout">
+      <a class="blue waves-effect waves-light right btn disabled" onclick="selectTicketsPrice()">Proseguir para pagamento</a>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+    `); 
+
+    for(var i=0;i<size;i++){
+        document.getElementById("price").innerHTML+=( `
+            <option  value="${i+1}">${i+1}</option>
+        `);
+        document.getElementById("halfPrice").innerHTML+=( `
+            <option  value="${i+1}">${i+1}</option>
+        `);
+   
+    }
+  
+
+
+
+
+
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+      });
+      document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('select');
+        var instances = M.FormSelect.init(elems, options);
+      });
+    
+      // Or with jQuery
+    
+      $(document).ready(function () {
+        $('select').formSelect();
+      });
+      document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems, options);
+      });
+    
+      // Or with jQuery
+    
+      $(document).ready(function () {
+        $('.modal').modal();
+      });
+    
+    
+      document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.slider');
+        var instances = M.Slider.init(elems, options);
+      });
+}
+
+
 function setButton(){
     if(size != 0){
+        showSelectedTicket();
         document.getElementById("btnTicket").innerHTML=( `
-            <a class="blue waves-effect waves-light right btn"  onclick="selectTicketsPrice()">Tipo de Ingresso</a>    
+            <a class="waves-effect blue waves-light btn modal-trigger" href="#modal3" >Tipo de Ingresso</a>    
         `);
-        
         
     }
     else{
         document.getElementById("btnTicket").innerHTML=( `
-            <a class="blue waves-effect waves-light right btn disabled"  onclick="selectTicketsPrice()">Tipo de Ingresso</a>    
+        <a class="waves-effect blue waves-light btn modal-trigger disabled" href="#modal3" >Tipo de Ingresso</a>       
         `);
         document.getElementById("noneSelected").innerHTML=( `
             <h6 class="center">Nenhum</h6>
@@ -146,7 +352,7 @@ function setButton(){
 }
 
 function removeSelected(seat){
-    console.log(selectedArray);
+    //console.log(selectedArray);
     $("#array"+seat).remove();
 }
 
@@ -164,6 +370,10 @@ function selectSeat(seat){
         setButton();
     }
     if(image == "free.png"){
+        if(size >= 5){
+            M.toast({html: 'Você só pode selecionar no máximo 5 assentos por compra'});
+            return ;
+        }
         document.getElementById(seat).src = "selected.png";
         selectedArray[seat]=seat;
         printSelected(seat);
